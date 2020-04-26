7ce273d663e7e98f3358033142011a2606082b7e
Alexander Romaniuk <a.romanyuk@lantan.info>
2020-04-26 18:38:05 +0700

    renamed and moved files

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -8,6 +8,6 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
