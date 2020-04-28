0ab7d4da6831a22977adb2e8d9861a6ebcbb8141
Alexander Romaniuk <a.romanyuk@lantan.info>
2020-04-28 20:05:02 +0700

    finished horizontal scroll view

--- lib/components/image_scroll_view.dart
+++ lib/components/image_scroll_view.dart
@@ -1,38 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_simple_app/components/resized_asset_image.dart';
+import 'package:flutter_simple_app/constants/images_constants.dart';
 
 class ImageScrollView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.width/4,
+      height: MediaQuery.of(context).size.width / 4,
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(right: 10.0),
-            width: MediaQuery.of(context).size.width/4,
-            color: Colors.red,
-          ),
-          Container(
-            margin: EdgeInsets.only(right: 10.0),
-            width: MediaQuery.of(context).size.width/4,
-            color: Colors.blue,
-          ),
-          Container(
-            margin: EdgeInsets.only(right: 10.0),
-            width: MediaQuery.of(context).size.width/4,
-            color: Colors.green,
-          ),
-          Container(
-            margin: EdgeInsets.only(right: 10.0),
-            width: MediaQuery.of(context).size.width/4,
-            color: Colors.yellow,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width/4,
-            color: Colors.orange,
-          ),
+          ResizedAssetImage(APPLE_TREE_IMG),
+          ResizedAssetImage(GRAPES_TREE_IMG),
+          ResizedAssetImage(LEMON_TREE_IMG),
+          ResizedAssetImage(WATERMELON_TREE_IMG),
+          ResizedAssetImage(WHEAT_IMG),
         ],
       ),
     );
--- lib/constants/images_constants.dart
+++ lib/constants/images_constants.dart
@@ -1,2 +1,7 @@
 const MAIN_BACKGROUND_IMG = 'assets/images/main_background.jpg';
 const MAIN_LOGO_IMG = 'assets/images/main_logo.png';
+const APPLE_TREE_IMG = 'assets/images/apple_tree.jpg';
+const GRAPES_TREE_IMG = 'assets/images/grapes_tree.jpg';
+const LEMON_TREE_IMG = 'assets/images/lemon_tree.jpg';
+const WATERMELON_TREE_IMG = 'assets/images/watermelon_tree.jpg';
+const WHEAT_IMG = 'assets/images/wheat.jpg';
