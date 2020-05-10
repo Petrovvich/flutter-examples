320304a9ae7f0742bf390b43a4fafb5f82b19160
Alexander Romaniuk <a.romanyuk@lantan.info>
2020-05-10 23:07:38 +0700

    created module for restaurant app

---
+++ simple_restaurant_app/android/app/src/main/kotlin/ru/petrovich/simple_restaurant_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.petrovich.simple_restaurant_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ simple_restaurant_app/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(RestaurantApp());
+
+class RestaurantApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Container());
+  }
+}
